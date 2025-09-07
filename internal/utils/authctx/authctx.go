621f09a7a314aee6040b61a1833420bcb9becca3
mohits-git <mohitkhatri2805@gmail.com>
2025-09-07 11:50:03 +0530

    feat: add authctx utils package for authentication context helper

---
+++ internal/utils/authctx/authctx.go
@@ -0,0 +1,37 @@
+package authctx
+
+import (
+	"context"
+
+	"github.com/mohits-git/food-ordering-system/internal/domain"
+)
+
+type userKeyType string
+
+const userKey userKeyType = "user"
+
+type UserClaims struct {
+  UserID int
+  Role   domain.UserRole
+}
+
+func NewUserClaims(userID int, role domain.UserRole) UserClaims {
+  return UserClaims{
+    UserID: userID,
+    Role:   role,
+  }
+}
+
+func WithUserClaims(ctx context.Context, claims *UserClaims) context.Context {
+  return context.WithValue(ctx, userKey, claims)
+}
+
+func UserClaimsFromCtx(ctx context.Context) (*UserClaims, bool) {
+  val := ctx.Value(userKey)
+  if val == nil {
+    return nil, false
+  }
+
+  claims, ok := val.(*UserClaims)
+  return claims, ok
+}
