661de7acfd184c323c9106583fadd58415c213e0
mohits-git <mohitkhatri2805@gmail.com>
2025-09-08 00:45:21 +0530

    feat: test bcrypt pkg

--- internal/adapters/bcrypt/bcrypt.go
+++ internal/adapters/bcrypt/bcrypt.go
@@ -6,42 +6,42 @@ import (
 )
 
 type BcryptPasswordHasher struct {
-  cost int
+	cost int
 }
 
 func NewBcryptPasswordHasher(cost int) *BcryptPasswordHasher {
-  if cost < bcrypt.MinCost {
-    cost = bcrypt.MinCost
-  } else if cost > bcrypt.MaxCost {
-    cost = bcrypt.MaxCost
-  }
-  return &BcryptPasswordHasher{
-    cost: cost,
-  }
+	if cost < bcrypt.MinCost {
+		cost = bcrypt.MinCost
+	} else if cost > bcrypt.MaxCost {
+		cost = bcrypt.MaxCost
+	}
+	return &BcryptPasswordHasher{
+		cost: cost,
+	}
 }
 
 func (h *BcryptPasswordHasher) HashPassword(password string) (string, error) {
-  hash, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
-  if err != nil {
-    if err == bcrypt.ErrPasswordTooLong {
-      return "", apperr.NewAppError(apperr.ErrInvalid, "password too long", err)
-    }
-    return "", err
-  }
-  return string(hash), nil
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
+	if err != nil {
+		if err == bcrypt.ErrPasswordTooLong {
+			return "", apperr.NewAppError(apperr.ErrInvalid, "password too long", err)
+		}
+		return "", err
+	}
+	return string(hash), nil
 }
 
 func (h *BcryptPasswordHasher) ComparePassword(hashedPassword, password string) (bool, error) {
-  err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-  if err != nil {
-    switch err {
-    case bcrypt.ErrMismatchedHashAndPassword:
-      return false, nil
-    case bcrypt.ErrHashTooShort:
-      return false, apperr.NewAppError(apperr.ErrInvalid, "hashed password too short", err)
-    case bcrypt.ErrPasswordTooLong:
-      return false, apperr.NewAppError(apperr.ErrInvalid, "password too long", err)
-    }
-  }
-  return true, nil
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		switch err {
+		case bcrypt.ErrMismatchedHashAndPassword:
+			return false, nil
+		case bcrypt.ErrHashTooShort:
+			return false, apperr.NewAppError(apperr.ErrInvalid, "hashed password too short", err)
+		default:
+			return false, err
+		}
+	}
+	return true, nil
 }
